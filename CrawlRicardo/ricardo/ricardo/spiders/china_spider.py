37050535738302c789e87b00e1838386707e1410
Yao <yeyaozhang@gmail.com>
2019-01-14 13:23:01 +0100

    scrapy crawl china

---
+++ CrawlRicardo/ricardo/ricardo/spiders/china_spider.py
@@ -0,0 +1,45 @@
+#!/home/yao/anaconda3/bin/python
+
+import scrapy
+from scrapy.selector import Selector
+import pickle
+import datetime
+import os
+import webbrowser
+
+class QuotesSpider(scrapy.Spider):
+    name = "china"
+    pickle_path = 'saved_items.p'
+
+
+
+
+    def start_requests(self):
+        BASE_URL = 'https://www.ricardo.ch/de/c/antiquites-et-arts-38399/chinesisch'
+        urls = [BASE_URL+'?page='+str(i) for i in range(1,20)]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        saved_items = pickle.load(open(self.pickle_path, 'rb')) if os.path.exists(self.pickle_path) else dict()
+        item_ids = Selector(response=response).xpath('//a[@class="ric-article"]/@data-ric-article-id').extract()
+        item_urls = ['https://www.ricardo.ch' + item for item in Selector(response=response).xpath('//a[@class="ric-article"]/@href').extract()]
+        item_image_urls = Selector(response=response).xpath('//a[@class="ric-article"]/div/div/img/@src').extract()
+
+        vals = zip(item_urls, item_image_urls)
+        new_pairs = dict(zip(item_ids, vals))
+
+        new_items = list()
+        for key, val in new_pairs.items():
+            if key not in saved_items:
+                new_items.append(val)
+                webbrowser.open(val[0])
+                saved_items[key] = val
+
+        filename = 'china-'+datetime.datetime.today().strftime('%Y-%m-%d')+'.txt'
+        with open(filename, 'a') as f:
+            for k,v in new_items:
+                f.write(k+'\t'+v+'\n')
+        self.log('Saved file %s' % filename)
+
+        pickle.dump(saved_items, open(self.pickle_path, 'wb'))
